7a853c3aa25f4d924ab87c4fdb401da376077f31
Francisco Ruiz <fruiz.rc@gmail.com>
2020-12-12 18:25:16 -0400

    Listo ejercicio de dot_product

--- toolkit_algoritms/week3/python_code/car_fueling.py
+++ toolkit_algoritms/week3/python_code/car_fueling.py
@@ -4,18 +4,19 @@ import sys
 
 def compute_min_refills(distance, tank, stops):
     count = 0
-    prev = 0
+    last_refill = 0
+    remain = 0
     stops.append(distance)
-    for stop in stops:
-        if(stop < tank):
-            prev = stop
-        else:
-            count += 1
-            tank += prev
 
-        if(tank >= distance):
-            return count
-    return -1
+    for i in range(len(stops) - 1):
+        remain = stops[i+1] - stops[i]
+        if(remain > tank):
+            return -1
+        if(stops[i+1] - last_refill > tank):
+            count += 1
+            last_refill = stops[i]
+
+    return count
 
 if __name__ == '__main__':
     d, m, _, *stops = map(int, sys.stdin.read().split())
