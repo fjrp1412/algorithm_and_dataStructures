e55f13189d59f05afdcf4dee8eb1ff70e4b0e10d
Francisco Ruiz <fruiz.rc@gmail.com>
2020-12-14 20:24:30 -0400

    iniciado el algoritmo de largest_number en python, sin embargo está dejado a medias, falta acabarlo

--- toolkit_algoritms/week3/python_code/largest_number.py
+++ toolkit_algoritms/week3/python_code/largest_number.py
@@ -3,15 +3,15 @@
 import sys
 
 def largest_number(a):
-    #write your code here
     res = ""
+    array = []
     for x in a:
-        res += x
-    return res
+        array.append(x)
+    array.sort(reverse=True)
+    return res.join(array)
 
 if __name__ == '__main__':
     input = sys.stdin.read()
     data = input.split()
     a = data[1:]
     print(largest_number(a))
-    
