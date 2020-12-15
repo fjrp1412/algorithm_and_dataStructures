bfb171409b998653871dc5f486ea921c29d0c079
Francisco Ruiz <fruiz.rc@gmail.com>
2020-12-14 23:03:03 -0400

    Agregado el codigo de binary search

---
+++ toolkit_algoritms/week4/python_code/exercises_not_examam/binary_search.py
@@ -0,0 +1,19 @@
+def binary_seach(array, lower, high, key):
+    if(high < lower):
+        print(f"The element is not in the array, but if was, it will be in the index {lower}")
+        return -1
+
+    medium = lower + (high - lower // 2)
+    if(array[medium] == key):
+        return medium
+    elif(key < array[medium]):
+        return binary_seach(array, lower, medium-1, key)
+    else:
+        return binary_seach(array, medium+1, high, key)
+
+
+if __name__ == '__main__':
+    array = [1,2,3,5,6,7,9,11,12]
+    print(array)
+    print(binary_seach(array, 0, len(array)-1, 8))
+
