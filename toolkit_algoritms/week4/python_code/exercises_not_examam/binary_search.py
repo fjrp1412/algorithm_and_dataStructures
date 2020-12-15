e32411e6370c77903db8b62a1546a68b10432982
Francisco Ruiz <fruiz.rc@gmail.com>
2020-12-14 23:17:04 -0400

    Agregada version iterativa del binary search

--- toolkit_algoritms/week4/python_code/exercises_not_examam/binary_search.py
+++ toolkit_algoritms/week4/python_code/exercises_not_examam/binary_search.py
@@ -12,8 +12,24 @@ def binary_seach(array, lower, high, key):
         return binary_seach(array, medium+1, high, key)
 
 
+def iterative_binary_search(array, lower, high,key):
+    while(high > lower):
+        medium = lower + (high-lower // 2)
+
+        if(array[medium] == key):
+            return medium
+
+        elif( key < array[medium]):
+            high = medium-1
+
+        else:
+            lower = medium+1
+    return -1
+
+
 if __name__ == '__main__':
     array = [1,2,3,5,6,7,9,11,12]
     print(array)
-    print(binary_seach(array, 0, len(array)-1, 8))
+    print(binary_seach(array, 0, len(array)-1, 3))
+    print(iterative_binary_search(array, 0, len(array)-1, 3))
 
