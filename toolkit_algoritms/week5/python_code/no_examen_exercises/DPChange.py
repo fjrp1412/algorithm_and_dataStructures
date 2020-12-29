faa47eff2a3f0fb79ce35deb9c43654a9d6d5625
Frnacisco <fruiz.rc@gmail.com>
2020-12-29 19:04:51 -0400

    Add the dynamic program for minimum change of coins

---
+++ toolkit_algoritms/week5/python_code/no_examen_exercises/DPChange.py
@@ -0,0 +1,27 @@
+def DPChange(money, coins):
+    #Create a array will save the change for the amount of coins in the range from 0 to money
+    MinNumCoins = [0]
+
+    for m in range(1, money+1):
+        #For default, will be a None value or "infinite"
+        MinNumCoins.append(None)
+
+
+        #For every value between 0 and money, will get the minimum amount of coins to complete that value
+        for i in range(0, len(coins)):
+            if(m >= coins[i]):
+                numCoins = MinNumCoins[m - coins[i]] + 1
+
+                if(not bool(MinNumCoins[m])):
+                    MinNumCoins[m] = numCoins
+                else:
+                    if(numCoins < MinNumCoins[m]):
+                        MinNumCoins[m] = numCoins
+
+    return MinNumCoins[money]
+
+if __name__ == "__main__":
+    money = 15
+    coins = [1, 5, 6]
+    print(DPChange(money, coins))
+
