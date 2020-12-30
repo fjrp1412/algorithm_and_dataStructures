47ad0cdd40fb7e8d97b52a632aeb6004e3240a04
Frnacisco <fruiz.rc@gmail.com>
2020-12-30 14:30:02 -0400

    changed for ternary operator for way1 and way2

--- toolkit_algoritms/week5/python_code/exam_exercises/primitive_calculator.py
+++ toolkit_algoritms/week5/python_code/exam_exercises/primitive_calculator.py
@@ -3,19 +3,17 @@ import sys
 import math
 
 def optimal_sequence(n):
+
     cache = [[],[1]]
     if(n == 1):
         return cache[1]
 
     for i in range(2, n+1):
-        way1 = (math.inf, 0)
-        way2 = (math.inf, 0)
-        way3 = (len(cache[i-1]) + 1, i-1)
+        way1 = ( (len(cache[i//3]) + 1, i // 3) ) if i % 3 == 0 else (math.inf, 0)
 
-        if(i % 3 == 0):
-            way1 = (len(cache[i//3]) + 1, i // 3)
-        if(i % 2 == 0):
-            way2 = (len(cache[i//2]) + 1, i // 2)
+        way2 = ( (len(cache[i//2]) + 1, i // 2) ) if i % 2 == 0 else (math.inf, 0)
+
+        way3 = (len(cache[i-1]) + 1, i-1)
 
         if(way1[0] <= way2[0] and way1[0] <= way3[0]):
             result = cache[way1[1]] + [i]
