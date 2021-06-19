2d6de3f09ef5b28f1d7845348bc110b49520f3e4
Francisco Ruiz <fruiz.rc@gmail.com>
2021-06-19 18:33:14 -0400

    ready tree_height algorithm. Still have to implement a iterative version for DFS

--- dataStructure/week1/python/exam/tree_height.py
+++ dataStructure/week1/python/exam/tree_height.py
@@ -4,8 +4,54 @@ import sys
 import threading
 
 
+class Node:
+    def __init__(self, value=None):
+        self._value = value
+        self._childrens = []
+
+    def add_children(self, children):
+        self._childrens.append(children)
+
+    def get_childrens(self):
+        return self._childrens
+
+    def get_value(self):
+        return self._value
+
+    def set_value(self, value):
+        self._value = value
+
+
+def DFS_recursive(root, depth):
+    childrens_depths = []
+    if len(root.get_childrens()) == 0:
+        return depth
+
+    for children in root.get_childrens():
+        #        print(f"children value: {children.get_value()}")
+        #        print(f"childrens len: {len(children.get_childrens())}")
+        childrens_depths.append(DFS_recursive(children, depth + 1))
+
+#    print(childrens_depths)
+    return (max(childrens_depths))
+
+
 def compute_height(n, parents):
-    pass
+    tree = []
+
+    for i, node in enumerate(parents):
+        tree.append(Node(i))
+        if node == -1:
+            root_index = i
+
+    for i, node in enumerate(parents):
+
+        if node == -1:
+            continue
+
+        tree[node].add_children(tree[i])
+
+    return DFS_recursive(tree[root_index], 0) + 1
 
 
 def main():
